6cdecf4373cb8436ca1162f451658417149058cb
David Rodríguez <drodriguez@excursionesparacruceros.com>
2025-05-19 23:44:42 +0200

    Creada migracion para crear la tabla "archers"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Archery Team Score</title>
+</head>
+
+<body>
+    <h1>Archery Team Score</h1>
+    <p>By Titu</p>
+    <hr />
+    <p><a href="{{ url('/arqueros') }}">Arqueros</a></p>
+    <p><a href="{{ url('/competiciones') }}">Competiciones</a></p>
+    <p><a href="{{ url('/nueva-competicion') }}">Nueva competición</a></p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/arqueros', function () {
+    return view('arqueros');
+});
+
+Route::get('/competiciones', function () {
+    return view('competiciones');
+});
+
+Route::get('/nueva-competicion', function () {
+    return view('nueva-competicion');
 });
